4ac3889afb3c58d7a51b38ef897252aad8570f6d
mohamadmorady412 <mohamadmorady412@gmail.com>
2024-10-11 11:16:59 +0330

    Update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -161,5 +161,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+# CSRF_COOKIE_SECURE = True
 
